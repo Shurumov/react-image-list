e1d0fa08a5f83aadd1c79391f2554e7ed6a8979a
Aleksei <alexei@0113@gmail.com>
2018-07-04 15:32:23 +0700

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -3,306 +3,381 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import './index.css';
 
-class PopUp extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			title: "",
-			url: ""
-		};
-		this.titleChange = this.titleChange.bind(this);
-		this.urlChange = this.urlChange.bind(this);
-	}
-	titleChange(e){
-		this.setState({
-			title: e.target.value
-		})
-		
-	}
-	urlChange(e){
-		this.setState({
-			url: e.target.value
-		})
-	}
-	render(){
-		return(
-			<div className="pop-up_wrapper">
-				<div className="pop-up">
-					<div className="pop-up__title">New image</div>
-						<InputTitle titleChange={this.titleChange}  />
-						<InputUrl urlChange ={this.urlChange} />
-					<div className="pop-up__btn-wrapper">
-						<ButtonCloseModal toggleModal={this.props.toggleModal}/>
-						<ButtonAddImage title={this.state.title} url={this.state.url} addImage={this.props.addImage} />
-					</div>
-				</div>
-			</div>
-		)
-	}
+class PopUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      url: '',
+    };
+    this.titleChange = this.titleChange.bind(this);
+    this.urlChange = this.urlChange.bind(this);
+  }
+
+  titleChange(e) {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+
+  urlChange(e) {
+    this.setState({
+      url: e.target.value,
+    });
+  }
+
+  render() {
+    const { titleChange, urlChange } = this;
+    const { toggleModal, addImage } = this.props;
+    const { title, url } = this.state;
+    return (
+      <div className="pop-up_wrapper">
+        <div className="pop-up">
+          <div className="pop-up__title">
+New image
+          </div>
+          <InputTitle titleChange={titleChange} />
+          <InputUrl urlChange={urlChange} />
+          <div className="pop-up__btn-wrapper">
+            <ButtonCloseModal toggleModal={toggleModal} />
+            <ButtonAddImage
+              title={title}
+              url={url}
+              addImage={addImage}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-PopUp.propTypes={
-	toggleModal: PropTypes.func,
-	addImage: PropTypes.func
-}
+PopUp.propTypes = {
+  toggleModal: PropTypes.func.isRequired,
+  addImage: PropTypes.func.isRequired,
+};
 
 
-class InputTitle extends React.Component {
-	render(){
-		return(
-			<div>
-				<input onChange={this.props.titleChange} id="title" placeholder="Title" type="text" />
-			</div>
-		)
-	}
-}
+const InputTitle = (props) => {
+  const { titleChange } = props;
+  return (
+    <div>
+      <input onChange={titleChange} id="title" placeholder="Title" type="text" />
+    </div>
+  );
+};
 
 InputTitle.propTypes = {
-	titleChange: PropTypes.func
-}
+  titleChange: PropTypes.func.isRequired,
+};
 
-class InputUrl extends React.Component {
-	render(){
-		return(
-			<div>
-				<input onChange={this.props.urlChange} id="url" placeholder="URL" type="text"/>
-			</div>
-		)
-	}
-}
+const InputUrl = (props) => {
+  const { urlChange } = props;
+  return (
+    <div>
+      <input onChange={urlChange} id="url" placeholder="URL" type="text" />
+    </div>
+  );
+};
 
 InputUrl.propTypes = {
-	urlChange: PropTypes.func
-}
+  urlChange: PropTypes.func.isRequired,
+};
 
-class ButtonCloseModal extends React.Component {
-	render(){
-		return <div className="btn-close" onClick={this.props.toggleModal}>Close</div>
-	}
-}
+const ButtonCloseModal = (props) => {
+  const { toggleModal } = props;
+  return (
+    <div
+      className="btn-close"
+      onClick={toggleModal}
+      onKeyPress={toggleModal}
+      tabIndex={0}
+      role="button"
+    >
+Close
+    </div>
+  );
+};
 
 ButtonCloseModal.propTypes = {
-	toggleModal: PropTypes.func
-}
+  toggleModal: PropTypes.func.isRequired,
+};
 
-class ButtonAddImage extends React.Component {
-	render(){
-		return(
-					<div className="btn-add" 
-							onClick={() => this.props.addImage(this.props.title, this.props.url, Math.random())}>
-							Add
-					</div>
-		)}
-}
+const ButtonAddImage = (props) => {
+  const { addImage, title, url } = props;
+  return (
+    <div
+      className="btn-add"
+      onClick={() => addImage(title, url, Date.now())}
+      onKeyPress={() => addImage(title, url, Date.now())}
+      tabIndex={0}
+      role="button"
+    >
+				Add
+    </div>
+  );
+};
 
 ButtonAddImage.propTypes = {
-	title: PropTypes.string,
-	url: PropTypes.string,
-	addImage: PropTypes.func
-}
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  addImage: PropTypes.func.isRequired,
+};
 
-class Header extends React.Component{
-	render(){
-		return( 
-			<header className="page-header" >
-				<div className="page-header__img"></div>
-				<div className="page-header__title">Images</div>
-			</header>
-		)
-	}
-}
-
-class ButtonOpenModal extends React.Component{
-	render(){
-		return (
-			<div onClick={this.props.toggleModal} className="btn add-new-img">
+const Header = () => (
+  <header className="page-header">
+    <div className="page-header__img" />
+    <div className="page-header__title">
+					Images
+    </div>
+  </header>
+);
+const ButtonOpenModal = (props) => {
+  const { toggleModal } = props;
+  return (
+    <div onClick={toggleModal} onKeyPress={toggleModal} tabIndex={0} role="button" className="btn add-new-img">
 				New
-			</div>
-		)
-	}
-}
+    </div>
+  );
+};
 
 ButtonOpenModal.propTypes = {
-	toggleModal: PropTypes.func
-}
+  toggleModal: PropTypes.func.isRequired,
+};
 
-class ListGallery extends React.Component{
-	render(){
-		const imagesArray = this.props.imagesArray.map((item) => {
-			return <ListGalleryItem 
-							appWidth={this.props.appWidth} 
-							title={item.title}  
-							url={item.url} 
-							id={item.id} 
-							key={item.id} 
-							removeItem = {this.props.removeItem}/>
-		});
-		return (
-			<div className="list-galery" >
-				{imagesArray}
-			</div>
-		)
-	}
-}
+const ListGallery = (props) => {
+  const { appWidth, removeItem, imagesArray } = props;
+  const imagesArrayNew = imagesArray.map(item => (
+    <ListGalleryItem
+      appWidth={appWidth}
+      title={item.title}
+      url={item.url}
+      id={item.id}
+      key={item.id}
+      removeItem={removeItem}
+    />
+  ));
+  return (
+    <div className="list-galery">
+      {imagesArrayNew}
+    </div>
+  );
+};
 
 ListGallery.propTypes = {
-	imagesArray: PropTypes.arrayOf(
-		PropTypes.shape({
-		title: PropTypes.string,
-		url: PropTypes.string,
-		id: PropTypes.number
-	})),
-	removeItem: PropTypes.func,
-	appWidth: PropTypes.number
-}
+  imagesArray: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  removeItem: PropTypes.func.isRequired,
+  appWidth: PropTypes.number,
+};
+ListGallery.defaultProps = {
+  appWidth: 704,
+};
 
-class ListGalleryItem extends React.Component{
-	constructor(props){
-		super(props)
-		this.state = {
-			mobileDelete: false
-		};
-		this.showMobileDelete = this.showMobileDelete.bind(this);
-		this.hiddenMobileDelete = this.hiddenMobileDelete.bind(this)
-	}
-	showMobileDelete(e){
-		this.setState({
-			mobileDelete: true
-		})
-		e.stopPropagation();
-	}
-	hiddenMobileDelete(){
-		this.setState({
-			mobileDelete: false
-		})
-	}
-	render(){
-		const id = this.props.id;
-		let mobileDelete;
-		if (this.state.mobileDelete && this.props.appWidth< 704){
-			mobileDelete = 
-			<div className="list-galery__item_mobile-delete_wrapper">
-				<div className="list-galery__item_mobile-delete_cancel" onClick={this.hiddenMobileDelete}>Cancel</div>
-				<div className="list-galery__item_mobile-delete" onClick={() => this.props.removeItem(id)}>Delete</div>
-			</div>
-		}
-		return(
-			<div className="list-galery__item" >
-				<ListGalleryItemHeader 
-					title = {this.props.title} 
-					removeItem = {this.props.removeItem}	
-					id = {this.props.id}
-				/>
-				<div className="list-galery__item-img">
-					<img src={this.props.url} alt="" onClick={this.showMobileDelete} />
-					{mobileDelete}
-				</div>
-			</div>
-		)
-	}
+
+class ListGalleryItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileDelete: false,
+    };
+    this.showMobileDelete = this.showMobileDelete.bind(this);
+    this.hiddenMobileDelete = this.hiddenMobileDelete.bind(this);
+  }
+
+  showMobileDelete(e) {
+    this.setState({
+      mobileDelete: true,
+    });
+    e.stopPropagation();
+  }
+
+  hiddenMobileDelete(e) {
+    this.setState({
+      mobileDelete: false,
+    });
+    e.stopPropagation();
+  }
+
+  render() {
+    const {
+      id, removeItem, title, url, appWidth,
+    } = this.props;
+    const { hiddenMobileDelete, showMobileDelete } = this;
+    const { mobileDelete } = this.state;
+    let mobileDeleteBlock;
+    if (mobileDelete && (appWidth < 704)) {
+      mobileDeleteBlock = (
+        <div className="list-galery__item_mobile-delete_wrapper">
+          <div
+            className="list-galery__item_mobile-delete_cancel"
+            onClick={hiddenMobileDelete}
+            onKeyPress={hiddenMobileDelete}
+            tabIndex={0}
+            role="button"
+          >
+						Cancel
+          </div>
+          <div
+            className="list-galery__item_mobile-delete"
+            onClick={() => removeItem(id)}
+            onKeyPress={() => removeItem(id)}
+            tabIndex={0}
+            role="button"
+          >
+						Delete
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="list-galery__item">
+        <ListGalleryItemHeader
+          title={title}
+          removeItem={removeItem}
+          id={id}
+        />
+        <div
+          className="list-galery__item-img"
+          onClick={showMobileDelete}
+          onKeyPress={showMobileDelete}
+          tabIndex={0}
+          role="button"
+        >
+          <img
+            src={url}
+            alt=""
+
+          />
+          {mobileDeleteBlock}
+        </div>
+      </div>
+    );
+  }
 }
 
 ListGalleryItem.propTypes = {
-	appWidth: PropTypes.number,
-	title: PropTypes.string,
-	url: PropTypes.string,
-	id: PropTypes.number,
-	removeItem: PropTypes.func
-}
+  appWidth: PropTypes.number,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  removeItem: PropTypes.func.isRequired,
+};
 
-class ListGalleryItemHeader extends React.Component {
-	render(){
-		const id = this.props.id
-		return(
-			<div className="list-galery__item-header">
-				<div className="list-galery__item-title">{this.props.title}</div>
-				<div className="list-galery__item_delete" onClick={() => this.props.removeItem(id)}>Delete</div>
-			</div>
-		) 
-	}
-}
+ListGalleryItem.defaultProps = {
+  appWidth: 704,
+};
+
+const ListGalleryItemHeader = (props) => {
+  const { id, title, removeItem } = props;
+  return (
+    <div className="list-galery__item-header">
+      <div className="list-galery__item-title">
+        {title}
+      </div>
+      <div
+        className="list-galery__item_delete"
+        onClick={() => removeItem(id)}
+        onKeyPress={() => removeItem(id)}
+        tabIndex={0}
+        role="button"
+      >
+					Delete
+      </div>
+    </div>
+  );
+};
 
 ListGalleryItemHeader.propTypes = {
-	title: PropTypes.string,
-	removeItem: PropTypes.func,
-	id: PropTypes.number
-}
+  title: PropTypes.string.isRequired,
+  removeItem: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
+};
 
-class App extends React.Component{
 
-	constructor(props){
-		super(props);
-		this.state = {
-			showModal: false, 
-			images: [
-				{
-					title: "Flower",
-					url: "http://bipbap.ru/wp-content/uploads/2017/09/Cool-High-Resolution-Wallpaper-1920x1080-640x360.jpg",
-					id: 1
-				},
-				{
-					title: "Mountain",
-					url: "http://www.radionetplus.ru/uploads/posts/2013-05/1369460621_panda-26.jpg",
-					id: 2
-				}
-			],
-			applicationWidth: null
-		};
-		this.toggleModal = this.toggleModal.bind(this);
-		this.addImageInArray = this.addImageInArray.bind(this);
-		this.removeImageFromArray = this.removeImageFromArray.bind(this);
-		this.appWidth = this.appWidth.bind(this);
-	}
-
-	appWidth(e) {
-		this.setState({
-			applicationWidth: e.target.width
-		})
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      images: [
+        {
+          title: 'Flower',
+          url: 'http://bipbap.ru/wp-content/uploads/2017/09/Cool-High-Resolution-Wallpaper-1920x1080-640x360.jpg',
+          id: 1,
+        },
+        {
+          title: 'Mountain',
+          url: 'http://www.radionetplus.ru/uploads/posts/2013-05/1369460621_panda-26.jpg',
+          id: 2,
+        },
+      ],
+      applicationWidth: null,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.addImageInArray = this.addImageInArray.bind(this);
+    this.removeImageFromArray = this.removeImageFromArray.bind(this);
+    this.appWidth = this.appWidth.bind(this);
   }
 
-	addImageInArray(title, url, id){
-		const newItem = {
-			title: title,
-			url: url,
-			id: id
-		};
+  appWidth(e) {
+    this.setState({
+      applicationWidth: e.target.width,
+    });
+  }
 
-		const images = this.state.images;
-		images.unshift(newItem)
+  addImageInArray(title, url, id) {
+    const newItem = {
+      title,
+      url,
+      id,
+    };
 
-		this.setState(prevState => ({
-      images: images
-    }));
-	}
+    const { images } = this.state;
+    images.unshift(newItem);
+    this.setState({
+      images,
+    });
+  }
 
-	removeImageFromArray(key){
-		const newArray = this.state.images.filter(function(item){
-			return item.id !== key;
-		});
-		this.setState({images: newArray})
-	}
+  removeImageFromArray(key) {
+    const { images } = this.state;
+    const newArray = images.filter(item => item.id !== key);
+    this.setState({ images: newArray });
+  }
 
-	toggleModal(){
-		this.setState({
-			showModal: !this.state.showModal
-		})
-	}
-	render(){
-		return(
-			<div className="app-wrapper" onLoad={this.appWidth} >
-				{this.state.showModal ? 
-					<PopUp toggleModal={this.toggleModal} addImage={this.addImageInArray} /> 
-					: null}
-				<Header  />
-				<ButtonOpenModal toggleModal={this.toggleModal} />
-				<ListGallery 
-					imagesArray={this.state.images} 
-					removeItem={this.removeImageFromArray} 
-					appWidth={this.state.applicationWidth}
-					/>
-			</div>
-		) 
-	}
+  toggleModal() {
+    const { showModal } = this.state;
+    this.setState({
+      showModal: !showModal,
+    });
+  }
+
+  render() {
+    const { images, applicationWidth, showModal } = this.state;
+    const {
+      appWidth, removeImageFromArray, toggleModal, addImageInArray,
+    } = this;
+    return (
+      <div className="app-wrapper" onLoad={appWidth}>
+        {showModal
+          ? <PopUp toggleModal={toggleModal} addImage={addImageInArray} />
+          : null}
+        <Header />
+        <ButtonOpenModal toggleModal={toggleModal} />
+        <ListGallery
+          imagesArray={images}
+          removeItem={removeImageFromArray}
+          appWidth={applicationWidth}
+        />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
